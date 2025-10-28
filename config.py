1c9ee4fdc68980c08850d22c9d5008ca40511cc8
Joe M Kariuki <mwanikijoe1@gmail.com>
2025-10-28 15:56:55 +0300

    SECURITY: Remove hardcoded credentials and add proper .gitignore

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class DevelopmentConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql://hotfix_user:UlNqxVgaEpb5aDMUKRQ97fZkwPn7LsSB@dpg-d3vie4ndiees73f0rtc0-a.oregon-postgres.render.com/hotfix'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 config = {
     'development': DevelopmentConfig,
